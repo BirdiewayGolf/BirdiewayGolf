00b5df52ac83fa92392e97a50c61900ca28d4f41
BirdiewayGolf <birdiewaygolf@gmail.com>
2024-12-12 11:49:00 -0700

    Remove LongDayRegistrationForm temporarily to fix build

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,38 +1,30 @@
 import { createBrowserRouter, Outlet } from 'react-router-dom';
-
 // Layout Components
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-
 // Public Pages
 import Home from './pages/public/Home';
 import About from './pages/public/About';
 import Contact from './pages/public/Contact';
 import Fundraising from './pages/public/Fundraising';
 import Sponsor from './pages/public/Sponsor';
-
 // Public Components
 import TournamentList from './components/public/TournamentList';
 import TournamentDetails from './components/public/TournamentDetails';
 import Standings from './components/public/Standings';
 import BusinessRegistrationForm from './components/public/registration/BusinessRegistrationForm';
 import JuniorRegistrationForm from './components/public/registration/JuniorRegistrationForm';
-import LongDayRegistrationForm from './components/public/registration/LongDayRegistrationForm';
-
 // Admin Pages
 import AdminDashboard from './pages/admin/AdminDashboard';
 import EditTournament from './pages/admin/EditTournament';
 import LoginPage from './pages/admin/LoginPage';
 import RegistrationsList from './pages/admin/RegistrationsList';
 import TournamentListAdmin from './pages/admin/TournamentList';
-
 // Admin Components
 import StandingsManager from './components/admin/StandingsManager';
 import TournamentForm from './components/admin/TournamentForm';
-
 // Auth Components
 import PrivateRoute from './components/auth/PrivateRoute';
-
 // 404 Page
 import NotFound from './pages/NotFound';
 
@@ -58,17 +50,13 @@ const router = createBrowserRouter([
       { path: 'contact', element: <Contact /> },
       { path: 'fundraising', element: <Fundraising /> },
       { path: 'sponsor', element: <Sponsor /> },
-
       // Tournament Routes
       { path: 'tournaments', element: <TournamentList /> },
       { path: 'tournaments/details/:id', element: <TournamentDetails /> },
       { path: 'standings', element: <Standings /> },
-
       // Registration Routes
       { path: 'register/business', element: <BusinessRegistrationForm /> },
       { path: 'register/junior', element: <JuniorRegistrationForm /> },
-      { path: 'register/longday', element: <LongDayRegistrationForm /> },
-
       // Admin Routes
       { path: 'admin/login', element: <LoginPage /> },
       { path: 'admin', element: <PrivateRoute><AdminDashboard /></PrivateRoute> },
